18a8af9cf97e1db1342c207d06b289fbc46637a8
EodyneDevelopAir <eodynedeveloper@gmail.com>
2023-11-19 19:02:02 +0100

    Add entry point for the program

---
+++ GravitationalFieldEstimator/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace GravitationalFieldEstimator
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please, provide in arguments.");
+                return;
+            }
+
+            Solver solver = new Solver(new GravitationStatementParser(args), 1);
+            solver.Initialize();
+            solver.Solve();
+        }
+    }
+}
---
+++ GravitationalFieldEstimator/Solver.cs
@@ -0,0 +1,37 @@
+﻿namespace GravitationalFieldEstimator
+{
+	public class Solver
+	{
+		private readonly float _gravitationalConstant;
+
+		private IStatementParser _statementParser;
+		private IGravitationalMovable _gravitationalMover;
+
+		public Solver(IStatementParser statementParser, float gravitationalConstant)
+		{
+			_statementParser = statementParser;
+			_gravitationalConstant = gravitationalConstant;
+		}
+
+		public void Initialize()
+		{
+			_statementParser.Parse();
+            GravitationalField field = new GravitationalField(_gravitationalConstant);
+            _gravitationalMover = new UniformGravitationalMover(field, 
+            _statementParser.Planet, _statementParser.Meteor);
+        }
+
+        public void Solve()
+        {
+            foreach (Vector2D timeEntry in _statementParser.TimeDistribution)
+            {
+                for (int i = 0; i < timeEntry.Y; i++)
+                {
+                    Vector2D estimatedPosition =
+                     _gravitationalMover.EstimatePosition(timeEntry.X);
+                    Console.WriteLine($"{estimatedPosition.X:F2} {estimatedPosition.Y:F2}");
+                }
+            }
+        }
+	}
+}
